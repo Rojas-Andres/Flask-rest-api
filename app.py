8c7f6309c1d0e499a613bae5ad905e17bc723eb3
Rojas-Andres <andresfelipe200004@gmail.com>
2021-10-22 19:32:09 -0500

    Guardando token en sesion flask

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,jsonify,request
+from flask import Flask,jsonify,request,render_template,redirect,url_for,session
 from db import Session , engine,connection_db
 import json
 from werkzeug.security import generate_password_hash,check_password_hash
@@ -7,6 +7,8 @@ from functools import wraps
 from flask_sqlalchemy import SQLAlchemy
 import jwt
 from api.controllers import bp_api
+import requests
+
 app = Flask(__name__)
 app.config['SECRET_KEY']='Th1s1ss3cr3t'
 
@@ -15,7 +17,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = connection_db
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
-session = Session()
+session_bd = Session()
 
 from models import *
 app.register_blueprint(bp_api)
@@ -23,46 +25,66 @@ app.register_blueprint(bp_api)
 def token_required(f):
    @wraps(f)
    def decorator(*args, **kwargs):
-        token = None
-        if 'x-access-tokens' in request.headers:
-            token = request.headers['x-access-tokens']
-
-        if not token:
-            return jsonify({'message': 'No has enviado el token!'})
-
         try:
-            data = jwt.decode(token, app.config['SECRET_KEY'])
-            #current_user = Users.query.filter_by(public_id=data['public_id']).first()
+            data = jwt.decode(session["token"], app.config['SECRET_KEY'])
+            print("Excelente")
         except:
-            return jsonify({'message': 'El token no es valido!'})
-        return f(data['public_id'],*args, **kwargs)
+            print("El token no es valido o no esta logueado")
+            return redirect(url_for('login'))
+        return f(*args, **kwargs)
    return decorator
-@app.route('/login',methods=['GET'])
-def login():
-    auth = request.authorization   
-
-    if not auth or not auth.username or not auth.password:  
-        return jsonify({"Repuesta":"No se puede verificar"})
-    
-    with engine.connect() as con:
-        user = con.execute(f"select * from usuario where username='{auth.username}'").one()
-    print(user)       
-    if check_password_hash(user[2], auth.password):  
-        token = jwt.encode({'public_id': user[1], 'exp' : datetime.datetime.utcnow() + datetime.timedelta(minutes=30)}, app.config['SECRET_KEY'])  
-        return jsonify({'token' : token.decode('UTF-8')}) 
-    else:
-
-        return jsonify({"respuesta": "Contraseña incorrecta!"})
 
 @app.route('/', methods=['GET'])
+@token_required
 def index():
-    return jsonify({'message': 'Hola este es el get'})
+    return render_template('index.html')
 
+@app.route('/logout', methods=['GET'])
+def logout():
+    session.clear()
+    return render_template('login.html')
+
+@app.route('/registro',methods=['GET','POST'])
+def registro():
+    if request.method=='POST':
+        username = request.form["username"]
+        password = request.form["password"]
+        args = {
+            "username":username,
+            "password":password
+        }
+        response = requests.post('http://localhost:5000/create_user',json=args)
+        if response.status_code == 200:
+            print("Entre" , response.status_code)
+            return render_template('index.html')
+        else:
+            return render_template('registro.html')
+    return render_template('registro.html')
+
+@app.route('/login',methods=['GET','POST'])
+def login():
+    if request.method=='POST':
+        username = request.form["username"]
+        password = request.form["password"]
+        print(username,password)
+        args = {
+            "username":username,
+            "password":password
+        }
+        response = requests.post('http://localhost:5000/Api/Methods/Login/',json=args)
+
+        print(response.status_code)
+        if response.status_code == 200:
+            response_api = json.loads(response.text)
+            print(response_api)
+            session["token"] = response_api["token"]
+            return render_template('index.html')
+        else:
+            return render_template('login.html')
+    return render_template('login.html')
 
 @app.route('/create_user',methods=['POST'])
 def crear_usuario():
-    #print(current_user)
-    #if current_user == "administrador":
     data = json.loads(request.data)
     if 'username' not in data :
         return jsonify({"respuesta":"No estas enviando el username!"})
@@ -78,9 +100,9 @@ def crear_usuario():
     with engine.connect() as con:
         hash_password = generate_password_hash(data["password"],method="sha256")
         nuevo_usuario = Usuario(username=data["username"],password=hash_password)
-        session.add(nuevo_usuario)
+        session_bd.add(nuevo_usuario)
         try:
-            session.commit()
+            session_bd.commit()
         except:
             return jsonify({"respuesta":"Usuario ya esta creado en la base de datos!"})
     return jsonify({"respuesta":"Usuario creado correctamente!"})
